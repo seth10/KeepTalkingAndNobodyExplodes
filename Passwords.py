cbb7879d6c3a74d7739b76b09709819e274d4854
Seth T <seth10@users.noreply.github.com>
2022-02-18 15:38:11 -0800

    Simplify letter options output using zip_longest

--- Passwords.py
+++ Passwords.py
@@ -1,9 +1,14 @@
+import itertools
+
 class Passwords:
   def __init__(self, password_options):
     self.password_options = password_options
     self.password_length = max(map(len, self.password_options))
   
   def letter_options_by_column(self):
+    """
+    Returns a nested list. Each item in the root list is a list of letters that can appear in that column. i.e. The first list is the letters that can show up in the first position, the second list is the letters that can appear in the second position, etc. Each list is sorted.
+    """
     letter_options = []
     for column_number in range(self.password_length):
       options = set()
@@ -16,12 +21,6 @@ class Passwords:
 if __name__ == "__main__":
   standard_password_module = Passwords(["about", "after", "again", "below", "could", "every", "first", "found", "great", "house", "large", "learn", "never", "other", "place", "plant", "point", "right", "small", "sound", "spell", "still", "study", "their", "there", "these", "thing", "think", "three", "water", "where", "which", "world", "would", "write"])
   letter_options = standard_password_module.letter_options_by_column()
-  most_options = max(map(len, letter_options))
-  for row in range(most_options):
-    for column in range(len(letter_options)):
-      try:
-        print(letter_options[column][row], end='  ')
-      except IndexError:
-        print(' ', end='  ')
-    print()
+  for row in itertools.zip_longest(*letter_options, fillvalue=' '):
+    print('  '.join(row))
     
